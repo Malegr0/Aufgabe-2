c07f1ede69fcb1ed4c0158ef0ea14acc74ba7787
Malegr0 <schoening.andreas@gmx.de>
2021-02-14 22:33:14 +0100

    variables, constructor and setter added to ComplexNumbers.h

--- Aufgabe 2/ComplexNumber.h
+++ Aufgabe 2/ComplexNumber.h
@@ -1,1 +1,29 @@
 #pragma once
+
+class ComplexNumbers {
+private:
+	float m_imag;
+	float m_real;
+	float m_absolute_amount;
+	float m_angle;
+
+public:
+	ComplexNumbers(char inputType, float valueA, float valueB);
+
+	void setMImag(float imag) {
+		this->m_imag = imag;
+	}
+
+	void setMReal(float real) {
+		this->m_real = real;
+	}
+
+	void setMAbsoluteAmount(float absoluteAmount) {
+		this->m_absolute_amount = absoluteAmount;
+	}
+
+	void setMAngle(float angle) {
+		this->m_angle = angle;
+	}
+
+};
