53fce9a2ad1e988a6e4099fd40c54f28a3db1918
Malegr0 <schoening.andreas@gmx.de>
2021-02-15 11:23:30 +0100

    added algorithm to toString methods

--- Aufgabe 2/ComplexNumber.cpp
+++ Aufgabe 2/ComplexNumber.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cmath>
+#include <string>
 #include "ComplexNumber.h"
 
 ComplexNumber::ComplexNumber(char inputType, float valueA, float valueB) {
@@ -33,17 +34,21 @@ float ComplexNumber::calculateRealWithPolar(float absoluteAmount, float angle) {
 std::string ComplexNumber::toCartesianString() {
 	if (this->initialInputType == 'c') {
 		//if already in cartesian form then just output both variables
+		return "z = " + std::to_string(this->valueA) + " + " + std::to_string(this->valueB) + "i";
 	}
 	else {
 		//if not in cartesian form then calculate values for output
+		return "z = " + std::to_string(calculateRealWithPolar(this->valueA, this->valueB)) + " + " + std::to_string(calculateImagWithPolar(this->valueA, this->valueB)) + "i";
 	}
 }
 
 std::string ComplexNumber::toPolarString() {
 	if (this->initialInputType == 'p') {
 		//if already in polar form then just output both variables
+		return "z = " + std::to_string(this->valueA) + " *e^( " + std::to_string(this->valueB) + "i)";
 	}
 	else {
 		//if not in polar form then calculate values for output
+		return "z = " + std::to_string(calculateAbsoluteAmountWithCoord(this->valueA, this->valueB)) + " * e^( " + std::to_string(calculateAngleWithCoord(this->valueA, this->valueB, calculateAbsoluteAmountWithCoord(this->valueA, this->valueB))) + "i)";
 	}
 }
