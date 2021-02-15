97dac6efe08aae335625a8ce84695c86096b75cc
Malegr0 <schoening.andreas@gmx.de>
2021-02-15 11:28:47 +0100

    deleted this calls

--- Aufgabe 2/ComplexNumber.cpp
+++ Aufgabe 2/ComplexNumber.cpp
@@ -32,23 +32,23 @@ float ComplexNumber::calculateRealWithPolar(float absoluteAmount, float angle) {
 }
 
 std::string ComplexNumber::toCartesianString() {
-	if (this->initialInputType == 'c') {
+	if (initialInputType == 'c') {
 		//if already in cartesian form then just output both variables
-		return "z = " + std::to_string(this->valueA) + " + " + std::to_string(this->valueB) + "i";
+		return "z = " + std::to_string(valueA) + " + " + std::to_string(valueB) + "i";
 	}
 	else {
 		//if not in cartesian form then calculate values for output
-		return "z = " + std::to_string(calculateRealWithPolar(this->valueA, this->valueB)) + " + " + std::to_string(calculateImagWithPolar(this->valueA, this->valueB)) + "i";
+		return "z = " + std::to_string(calculateRealWithPolar(valueA, valueB)) + " + " + std::to_string(calculateImagWithPolar(valueA, valueB)) + "i";
 	}
 }
 
 std::string ComplexNumber::toPolarString() {
-	if (this->initialInputType == 'p') {
+	if (initialInputType == 'p') {
 		//if already in polar form then just output both variables
-		return "z = " + std::to_string(this->valueA) + " *e^( " + std::to_string(this->valueB) + "i)";
+		return "z = " + std::to_string(valueA) + " *e^( " + std::to_string(valueB) + "i)";
 	}
 	else {
 		//if not in polar form then calculate values for output
-		return "z = " + std::to_string(calculateAbsoluteAmountWithCoord(this->valueA, this->valueB)) + " * e^( " + std::to_string(calculateAngleWithCoord(this->valueA, this->valueB, calculateAbsoluteAmountWithCoord(this->valueA, this->valueB))) + "i)";
+		return "z = " + std::to_string(calculateAbsoluteAmountWithCoord(valueA, valueB)) + " * e^( " + std::to_string(calculateAngleWithCoord(valueA, valueB, calculateAbsoluteAmountWithCoord(valueA, valueB))) + "i)";
 	}
 }
